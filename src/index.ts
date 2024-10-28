22f11d10d441e5a59a4f621d88348e0a77dc1aeb
Sayyed Jamal Ghasemi <jamal13647850@gmail.com>
2024-10-28 15:40:19 +0330

    second commit

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 export default function (Alpine) {
-    Alpine.directive('trim', (el, { modifiers }) => {
-        let count = 5;
-        const index = modifiers.indexOf('count');
-        if (index !== -1 && index + 1 < modifiers.length) {
-            count = modifiers[index + 1];
-        }
-        el.innerHTML = el.innerHTML.length > count ? el.innerHTML.substring(0, count) + '...' : el.innerHTML;
-    })
-    
+  Alpine.directive('trim', (el, { modifiers }) => {
+    let count = 5;
+    const index = modifiers.indexOf('count');
+    if (index !== -1 && index + 1 < modifiers.length) {
+      count = modifiers[index + 1];
+    }
+
+    el.innerHTML =
+      el.innerHTML.length > count
+        ? el.innerHTML.substring(0, count) + '...'
+        : el.innerHTML;
+  });
 }

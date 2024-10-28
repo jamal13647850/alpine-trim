d88a9dc57ed4ef44e6d0d0aba68165cb6f345635
Sayyed Jamal Ghasemi <jamal13647850@gmail.com>
2024-10-28 15:09:32 +0330

    First commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export default function (Alpine) {
+    Alpine.directive('trim', (el, { modifiers }) => {
+        let count = 5;
+        const index = modifiers.indexOf('count');
+        if (index !== -1 && index + 1 < modifiers.length) {
+            count = modifiers[index + 1];
+        }
+        el.innerHTML = el.innerHTML.length > count ? el.innerHTML.substring(0, count) + '...' : el.innerHTML;
+    })
+    
+}
